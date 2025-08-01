023d341be533c2d910a063bacf30c312846fc59b
Mark Westerlund <mana.torsk@gmail.com>
2025-08-01 15:14:43 +0200

    added visual indicator and reset button

--- Xenon/Mods/TimeController.cs
+++ Xenon/Mods/TimeController.cs
@@ -8,15 +8,29 @@ namespace Xenon.Mods
         private static float currentScale = 1f;
         private const float scaleStep = 0.05f;
 
+        private GUIStyle _style;
+
+
         void Awake()
         {
             Debug.Log("Scale " + currentScale);
             RM.time.SetTargetTimescale(currentScale);
+            _style = new()
+            {
+                fontSize = 36,
+                fontStyle = FontStyle.Bold
+            };
+            _style.normal.textColor = Color.gray;
+
         }
 
         void Update()
         {
-            if (InputManager.GetKeyDown(Settings.speedUp.Value))
+            if (InputManager.GetKeyDown(Settings.speedReset.Value))
+            {
+                Reset();
+            }
+            else if (InputManager.GetKeyDown(Settings.speedUp.Value))
             {
                 float newTime = RM.time.GetCurrentTimeScale() + scaleStep;
                 if (newTime > 2f) return;
@@ -27,7 +41,7 @@ namespace Xenon.Mods
             else if (InputManager.GetKeyDown(Settings.speedDown.Value))
             {
                 float newTime = RM.time.GetCurrentTimeScale() - scaleStep;
-                if (newTime <= 0.15f) return;
+                if (newTime < 0.10f) return;
 
                 RM.time.SetTargetTimescale(newTime);
                 currentScale = newTime;
@@ -35,5 +49,13 @@ namespace Xenon.Mods
         }
 
         public static void Reset() => currentScale = 1f;
+
+        private void OnGUI()
+        {
+            if(!(RM.mechController.GetIsAlive() && currentScale != 1f && Settings.speedShow.Value)) return;
+
+            GUI.Label(new Rect(Camera.main.pixelWidth - 600, 10, 100, 50), $"{currentScale:P0}", _style);
+
+        }
     }
 }
